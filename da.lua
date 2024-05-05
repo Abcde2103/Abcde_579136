284fd8c98e928fbb020a21215f4ca105a457d1c9
Abcde2103 <nxwhear@gmail.com>
2024-05-05 10:45:47 +0700

    Update da.lua

--- da.lua
+++ da.lua
@@ -1,1 +1,15 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/I4MMO/Detazy/main/Main', true))()
+if game.PlaceId == 2753915549 then
+    F1 = true
+elseif game.PlaceId == 4442272183 then
+    F2 = true
+elseif game.PlaceId == 7449423635 then
+    F3 = true
+elseif game.PlaceId == 3101667897 then
+    L = true
+elseif game.PlaceId == 10704789056 then
+    B = true
+elseif game.PlaceId == 537413528 then
+    D = true
+elseif game.PlaceId == 10704789056 then
+else game:GetService("Players").LocalPlayer:Kick("Not Support Map 🚫")
+end
